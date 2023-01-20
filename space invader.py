d55583de004fe390b23be30be079bd35fdf60a38
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2023-01-19 20:48:20 -0700

    Update space invader.py

--- space invader.py
+++ space invader.py
@@ -7,11 +7,18 @@ screen = pygame.display.set_mode((1000, 800))  # creates game screen
 screen.fill((0,0,0))
 clock = pygame.time.Clock() #set up clock
 
-#player
-keys = [False, False, False]
+
 #CONSTANTS
 LEFT=0
 RIGHT=1
+
+#player variables
+xpos = 430 #xpos of player
+ypos = 750 #ypos of player
+vx = 0 #x velocity of player
+keys = [False, False, False]
+
+
 
 
 class Enemy:
@@ -32,33 +39,11 @@ class Enemy:
             pygame.draw.arc(screen, ((22, 100, 8)), (self.xpos+45, self.ypos-51, 110, 115), (5*math.pi)/6, (math.pi), 8)
             pygame.draw.ellipse(screen, ((0, 0, 0)), (self.xpos, self.ypos, 100, 80), 5)
 
-class spaceship:
-    def __init__(self, xpos, ypos):
-        self.xpos = xpos
-        self.ypos = ypos
-        self.alive = True
-        self.vx = 0
 
-    def move(self):
-        if keys[0]==True:
-            vx=3
-        elif keys[1]==True:
-            vx =-3
-        else:
-            vx = 0
-
-        xpos+=vx
-            
-    def draw(self):
-        if self.alive is True:
-            pygame.draw.rect(screen, (0, 255,0), (self.xpos,self.ypos, 100, 20))
-
-#instantiate a spaceship object from the class
-idk = spaceship(450,750)
-
-while True:  
-    clock.tick(60)
-
+while not gameover: #GAME LOOP############################################################
+    clock.tick(60) #FPS
+    
+    #Input Section------------------------------------------------------------
     for event in pygame.event.get(): #quit game if x is pressed in top corner
         if event.type == pygame.QUIT:
             gameover = True
@@ -69,24 +54,47 @@ while True:
 
             elif event.key == pygame.K_RIGHT:
                 keys[RIGHT]=True
-
-                #Player 1 Input
+          
+                    
+# Input KEYUP
+            
+        #Player 1 Input
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 keys[LEFT]=False
+            
+            if event.key == pygame.K_RIGHT:
+                keys[RIGHT]=False
+  
 
-                
-            elif event.key == pygame.K_RIGHT:
-                keys[RIGHT]=False 
+    #LEFT MOVEMENT
+    if keys[LEFT]==True:
+        vx=-3
+        direction = LEFT
+        
+  
 
-    #physics section-------------------------------------------
+    #RIGHT MOVEMENT
+    elif keys[RIGHT]==True:
+        vx=3
+        direction = RIGHT
 
+    #turn off velocity
+    else:
+        vx = 0
 
+    #update player position
+    xpos+=vx 
+  
+    # RENDER Section--------------------------------------------------------------------------------
+            
     screen.fill((0,0,0)) #wipe screen so it doesn't smear
-    idk.draw()
     
-    
+    #Player 1
+    pygame.draw.rect(screen, (0, 250,0), (xpos, ypos, 80, 20))
+  
 
     pygame.display.flip()#this actually puts the pixel on the screen
     
+#end game loop------------------------------------------------------------------------------
 pygame.quit()
