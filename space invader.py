7ae4a0df09e41654c1c9eb515783a34b44cc1da1
FlzGlz <51001772+richfls@users.noreply.github.com>
2023-01-23 09:50:24 -0700

    Update space invader.py

--- space invader.py
+++ space invader.py
@@ -1,24 +1,11 @@
-import pygame
+import pygame as py
 from math import *
 import time
-pygame.init()  
-pygame.display.set_caption("Space Invaders")  # sets the window title
-screen = pygame.display.set_mode((1000, 800))  # creates game screen
+py.init()  
+py.display.set_caption("Space Invaders")  # sets the window title
+screen = py.display.set_mode((1000, 800))  # creates game screen
 screen.fill((0,0,0))
-clock = pygame.time.Clock() #set up clock
-gameover = False #variable to run our game loop
-
-
-
-#CONSTANTS
-LEFT=0
-RIGHT=1
-
-#player variables
-xpos = 430 #xpos of player
-ypos = 750 #ypos of player
-vx = 0 #x velocity of player
-keys = [False, False, False]
+clock = py.time.Clock() #set up clock
 
 
 
@@ -33,70 +20,67 @@ class Enemy:
         self.color = (138 +random.randint(-10,10), 23 +random.randint(-10,10), 17 +random.randint(-10,10))
         self.color2 = (138-22, 23-12, 17-10)
         if self.alive is True:
-            pygame.draw.ellipse(screen, self.color, (self.xpos, self.ypos, 100, 80))
-            pygame.draw.arc(screen, self.color2, (self.xpos+10, self.ypos-16, 110, 115), (5*math.pi)/6, (7*math.pi)/6, 5)
-            pygame.draw.arc(screen, self.color2, (self.xpos+30, self.ypos-36, 110, 150), (5*math.pi)/6, (7*math.pi)/6, 5)
-            pygame.draw.arc(screen, self.color2, (self.xpos-22, self.ypos-16, 110, 115), (11*math.pi)/6, (math.pi)/6, 5)
-            pygame.draw.arc(screen, self.color2, (self.xpos-42, self.ypos-36, 110, 150), (11*math.pi)/6, (math.pi)/6, 5)
-            pygame.draw.arc(screen, ((22, 100, 8)), (self.xpos+45, self.ypos-51, 110, 115), (5*math.pi)/6, (math.pi), 8)
-            pygame.draw.ellipse(screen, ((0, 0, 0)), (self.xpos, self.ypos, 100, 80), 5)
+            py.draw.rect(screen,(255,255,255),())
 
+class spaceship:
+    def __init__(self, xpos):
+        self.xpos = xpos
+        self.alive = True
+        self.vx = 0
+        self.keys = [False, False, False]
+        self.LEFT=0
+        self.RIGHT=1
 
-while not gameover: #GAME LOOP############################################################
-    clock.tick(60) #FPS
-    
-    #Input Section------------------------------------------------------------
-    for event in pygame.event.get(): #quit game if x is pressed in top corner
-        if event.type == pygame.QUIT:
-            gameover = True
-      
-        if event.type == pygame.KEYDOWN: #keyboard input
-            if event.key == pygame.K_LEFT:
-                keys[LEFT]=True
-
-            elif event.key == pygame.K_RIGHT:
-                keys[RIGHT]=True
-          
-                    
-# Input KEYUP
-            
-        #Player 1 Input
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                keys[LEFT]=False
-            
-            if event.key == pygame.K_RIGHT:
-                keys[RIGHT]=False
-  
-
+    def move(self):
+        for event in py.event.get(): #quit game if x is pressed in top corner
+            if event.type == py.QUIT:
+                self.alive  = False
+            if event.type == py.KEYDOWN: #keyboard input
+                if event.key == py.K_LEFT:
+                    self.keys[self.LEFT]=True
+                elif event.key == py.K_RIGHT:
+                    self.keys[self.RIGHT]=True
+    # Input KEYUP
+            elif event.type == py.KEYUP:
+                if event.key == py.K_LEFT:
+                    self.keys[self.LEFT]=False
+                if event.key == py.K_RIGHT:
+                    self.keys[self.RIGHT]=False
     #LEFT MOVEMENT
-    if keys[LEFT]==True:
-        vx=-3
-        direction = LEFT
-        
-  
-
+        if self.keys[self.LEFT] == True:
+            self.vx=-3
+            direction = self.LEFT
     #RIGHT MOVEMENT
-    elif keys[RIGHT]==True:
-        vx=3
-        direction = RIGHT
-
+        elif self.keys[self.RIGHT]==True:
+            self.vx=3
+            direction = self.RIGHT
     #turn off velocity
-    else:
-        vx = 0
-
-    #update player position
-    xpos+=vx 
-  
-    # RENDER Section--------------------------------------------------------------------------------
+        else:
+            self.vx = 0
+        #update player position
+        self.xpos+=self.vx 
             
-    screen.fill((0,0,0)) #wipe screen so it doesn't smear
-    
-    #Player 1
-    pygame.draw.rect(screen, (0, 250,0), (xpos, ypos, 80, 20))
-  
+    def draw(self):
+        if self.alive is True:
+            py.draw.rect(screen, (0, 255,0), (self.xpos,600, 100, 20))
+    def update(self):
+        self.draw()
+        self.move()
 
-    pygame.display.flip()#this actually puts the pixel on the screen
+#instantiate a spaceship object from the class
+player = spaceship(200)
+
+while True:  
+    clock.tick(60)
+
     
-#end game loop------------------------------------------------------------------------------
-pygame.quit()
+
+    #physics section-------------------------------------------
+
+
+    screen.fill((0,0,0)) #wipe screen so it doesn't smear
+    player.update()
+    
+
+    py.display.flip()#this actually puts the pixel on the screen
+py.quit()
