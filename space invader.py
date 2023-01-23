ec87ad65a844f999adf0724b5d0afe5f5a65a716
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2023-01-23 09:56:08 -0700

    Update space invader.py

--- space invader.py
+++ space invader.py
@@ -30,12 +30,15 @@ class spaceship:
         self.keys = [False, False, False]
         self.LEFT=0
         self.RIGHT=1
+        self.SPACE = 2
 
     def move(self):
         for event in py.event.get(): #quit game if x is pressed in top corner
             if event.type == py.QUIT:
                 self.alive  = False
             if event.type == py.KEYDOWN: #keyboard input
+                if event.key == py.K_SPACE:
+                    self.keys[self.SPACE] = True
                 if event.key == py.K_LEFT:
                     self.keys[self.LEFT]=True
                 elif event.key == py.K_RIGHT:
@@ -46,6 +49,11 @@ class spaceship:
                     self.keys[self.LEFT]=False
                 if event.key == py.K_RIGHT:
                     self.keys[self.RIGHT]=False
+                if event.key == py.K_SPACE:
+                    self.keys[self.SPACE]= False
+    #Shooting
+        if self.keys[self.SPACE] == True:
+            print("Shooting")
     #LEFT MOVEMENT
         if self.keys[self.LEFT] == True:
             self.vx=-3
@@ -62,13 +70,13 @@ class spaceship:
             
     def draw(self):
         if self.alive is True:
-            py.draw.rect(screen, (0, 255,0), (self.xpos,600, 100, 20))
+            py.draw.rect(screen, (0, 255,0), (self.xpos,750, 100, 20))
     def update(self):
         self.draw()
         self.move()
 
 #instantiate a spaceship object from the class
-player = spaceship(200)
+player = spaceship(450)
 
 while True:  
     clock.tick(60)
