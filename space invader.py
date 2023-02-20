df6cfbd4dd251f4b780417309986754cf3842603
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2023-02-20 13:57:46 -0700

    Update space invader.py

--- space invader.py
+++ space invader.py
@@ -1,127 +1,191 @@
-import pygame as py
-import random
-from math import *
-import time
-py.init()  
-py.display.set_caption("Space Invaders")  # sets the window title
-screen = py.display.set_mode((1000, 800))  # creates game screen
+import pygame
+pygame.init()  
+pygame.display.set_caption("Space Invaders")  # sets the window title
+screen = pygame.display.set_mode((1000, 800))  # creates game screen
 screen.fill((0,0,0))
-clock = py.time.Clock() #set up clock
+clock = pygame.time.Clock() #set up clock
+gameover = False #variable to run our game loop
 
+#CONSTANTS
+LEFT=0
+RIGHT=1
+SPACE = 2
+shoot = False
+
+#player variables
+xpos = 430 #xpos of player
+ypos = 750 #ypos of player
+vx = 0 #x velocity of player
 timer = 0;
 
+keys = [False, False, False, False,False, False, False, False] #this list holds whether each key has been pressed
+#this list holds whether each key has been pressed
 
-class Enemy:
-    
-    def __init__(self, xpos, ypos):
+class Bullet:
+    def __init__(self,xpos,ypos):
         self.xpos = xpos
         self.ypos = ypos
-        self.alive = True
+        self.isAlive = False
+    
+    def move(self,xpos,ypos):
+        if self.isAlive == True:
+            self.ypos -= 5
+        if self.ypos < 0:
+            self.isAlive = False
+            self.xpos = xpos
+            self.ypos = ypos
+
+            
+    def draw(self):
+        pygame.draw.rect(screen,(250,250,250),(self.xpos,self.ypos,3,20))
+
+        return True
+
+    def collide(self,BulletX,BulletY):
+        if self.isAlive:
+            if BulletX > self.xpos:
+                if BulletX < self.xpos + 40:
+                    if BulletY < self.ypos + 40:
+                       if BulletY > self.ypos:
+                          print("hit")
+                          self.isAlive = False
+                          return False 
+        return True 
+
+bullet = Bullet(xpos+28,ypos)
+
+        
+class Alien:
+    def __init__(self,xpos,ypos):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.isAlive = True
         self.direction = 1
     
-    
-    def move(self, time):
-        
-        if timer % 800 == 0:
-            self.ypos += 100 #Moves down
-            self.direction *=-1 #Flips direction
-            return 0 #Resets time to 0
-        
-        
-        if time % 100 == 0:
-            self.xpos+=50*self.direction #Moves right 
-        
-        return time 
-            
-    
     def draw(self):
-        py.draw.rect(screen, (255, 255,255), (self.xpos,self.ypos, 40, 40))
+        pygame.draw.rect(screen,(250,250,250),(self.xpos,self.ypos, 40,40))
+    
+    def move(self,time):
         
+        if time % 800 == 0:
+            self.ypos += 100
+            self.direction *=-1
+            return 0
+    
+        if timer %100 == 0:
+            self.xpos += 35*self.direction
+        
+        return time
+    
             
+               
+
 bob = []
+for i in range(4):
+    for j in range(10):
+        bob.append(Alien(j*70+50, i*70+30))
+        
+   
 
-for i in range(4): #Rows 
-    for j in range(10): #Columns
-        bob.append(Enemy(j*60+45, i*60+40))
+while not gameover: #GAME LOOP------------------------------------------------
+    clock.tick(100) #FPS
+    timer += 1
     
-class spaceship:
-    def __init__(self, xpos):
-        self.xpos = xpos
-        self.alive = True
-        self.vx = 0
-        self.keys = [False, False, False]
-        self.LEFT= 0
-        self.RIGHT=1
-        self.SPACE = 2
+    
+    
+    #Input Section------------------------------------------------------------
+    for event in pygame.event.get(): #quit game if x is pressed in top corner
+        if event.type == pygame.QUIT:
+            gameover = True
+      
+        if event.type == pygame.KEYDOWN: #keyboard input
+            if event.key == pygame.K_LEFT:
+                keys[LEFT]=True
 
-    def move(self):
-        for event in py.event.get(): #quit game if x is pressed in top corner
-            if event.type == py.QUIT:
-                self.alive  = False
-            if event.type == py.KEYDOWN: #keyboard input
-                if event.key == py.K_SPACE:
-                    self.keys[self.SPACE] = True
-                if event.key == py.K_LEFT:
-                    self.keys[self.LEFT]=True
-                elif event.key == py.K_RIGHT:
-                    self.keys[self.RIGHT]=True
-    # Input KEYUP
-            elif event.type == py.KEYUP:
-                if event.key == py.K_LEFT:
-                    self.keys[self.LEFT]=False
-                if event.key == py.K_RIGHT:
-                    self.keys[self.RIGHT]=False
-                if event.key == py.K_SPACE:
-                    self.keys[self.SPACE]= False
-    #Shooting
-        if self.keys[self.SPACE] == True:
-            print("shooting")
+            if event.key == pygame.K_SPACE:
+                keys[SPACE]=True
+                shoot = True
+
+            elif event.key == pygame.K_RIGHT:
+                keys[RIGHT]=True
+          
+                    
+# Input KEYUP
             
-    #LEFT MOVEMENT
-        if self.keys[self.LEFT] == True:
-            self.vx=-3
-            direction = self.LEFT
-    #RIGHT MOVEMENT
-        elif self.keys[self.RIGHT]==True:
-            self.vx=3
-            direction = self.RIGHT
-    #turn off velocity
-        else:
-            self.vx = 0
-        #update player position
-        self.xpos+=self.vx 
+        #Player 1 Input
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                keys[LEFT]=False
             
-    def draw(self):
-        if self.alive is True:
-            py.draw.rect(screen, (0, 255,0), (self.xpos,750, 100, 20))
+            if event.key == pygame.K_RIGHT:
+                keys[RIGHT]=False
+
+            if event.key == pygame.K_SPACE:
+                keys[SPACE]= False
+                shoot = False
+
             
-    def update(self):
-        self.draw()
-        self.move()
+   
+
   
 
-#instantiate a spaceship object from the class
-player = spaceship(450)
-
-
-
-
-while True:  
-    clock.tick(60)
-    timer += 1
-
+    #LEFT MOVEMENT
+    if keys[LEFT]==True:
+        vx=-3
+        direction = LEFT
     
 
-    #physics section-------------------------------------------
+    #RIGHT MOVEMENT
+    elif keys[RIGHT]==True:
+        vx=3
+        direction = RIGHT
+
+    #turn off velocity
+    else:
+        vx = 0
 
 
+    #update player position
+    xpos+=vx
+    
+    
+    #Physics section--------------------------------------------------------------
+    for i in range(len(bob)):
+        bob[i].move(timer)     
+
+
+  
+    # RENDER Section--------------------------------------------------------------------------------
+            
     screen.fill((0,0,0)) #wipe screen so it doesn't smear
-    player.update()
     
-    for i in range (len(bob)):
-        timer = bob[i].move(timer)
-        bob[i].draw()
     
+    #Player 1
+    pygame.draw.rect(screen, (0, 250,0), (xpos, 750, 60, 20))
+    pygame.draw.rect(screen, (0, 250,0), (xpos+5, 745, 50, 20))
+    pygame.draw.rect(screen, (0, 250,0), (xpos+25, 736, 10, 20))
+    pygame.draw.rect(screen, (0, 250,0), (xpos+28, 732, 4, 20))
 
-    py.display.flip()#this actually puts the pixel on the screen
-py.quit()
+    #shoot bullet
+    if shoot == True:
+        bullet.isAlive = True
+
+        if bullet.isAlive == True:
+            bullet.move(xpos+28, ypos)
+
+         
+    
+    #Bullet call
+    bullet.draw()
+  
+
+    #Enemy call function
+    for i in range (len(bob)):
+        bob[i].draw()
+
+  
+
+    pygame.display.flip()#this actually puts the pixel on the screen
+    
+#end game loop------------------------------------------------------------------------------
+pygame.quit()
